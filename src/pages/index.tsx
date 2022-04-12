540b2dd6425afdad1de14da2e5fac2b837656a04
wilyyy <walvarez1@my.bcit.ca>
2022-04-11 19:25:46 -0700

    components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import ButtonTest from '../components/ButtonTest';
 
 const Home: NextPage = () => {
    return (
       <div className="w-screen h-screen flex justify-center items-center">
-         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Button
-         </button>
+         <ButtonTest />
       </div>
    );
 };
